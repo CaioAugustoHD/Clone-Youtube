8f8e20f14ec585b21d92ce0c873055490191feb0
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-11-08 10:06:59 -0300

    Criado componente Header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,40 @@
+import config from "../config.json";
+import styled from "styled-components";
+
+
+const StyledHeader = styled.header`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    width: 100%;
+    padding: 16px 32px;
+  }
+`;
+function Header(){
+  return (
+    <StyledHeader>
+      <img src="" alt="Banner"/>
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} alt="Foto de perfil"/>
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  )
+}
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return (
+      <Header/>
+    )
   }
   
   export default HomePage
