79029042495eec2ee934a8d10a232d01182d826e
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-11-09 09:11:24 -0300

    Criado componente do conteúdo principal (TimeLine)

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,12 +16,12 @@ const StyledHeader = styled.header`
     padding: 16px 32px;
   }
 `;
-function Header(){
+function Header() {
   return (
     <StyledHeader>
-      <img src="" alt="Banner"/>
+      <img src="" alt="Banner" />
       <section className="user-info">
-        <img src={`https://github.com/${config.github}.png`} alt="Foto de perfil"/>
+        <img src={`https://github.com/${config.github}.png`} alt="Foto de perfil" />
         <div>
           <h2>{config.name}</h2>
           <p>{config.job}</p>
@@ -31,10 +31,39 @@ function Header(){
   )
 }
 
+function TimeLine({ playlists }) {
+  const playlistName = Object.keys(playlists);
+  return (
+    <main>
+      {playlistName.map((playlist) => {
+        const videos = playlists[playlist];
+        return (
+          <section>
+            <h2>{playlist}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                )
+                })}
+            </div>
+          </section>
+        )
+      })}
+    </main>
+  )
+}
+
 function HomePage() {
-    return (
-      <Header/>
-    )
-  }
-  
-  export default HomePage
+  return (
+    <>
+      <Header />
+      <TimeLine playlists={config.playlists} />
+    </>
+  )
+}
+
+export default HomePage
